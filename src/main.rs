c375e39d3640242f78b273c955f9ee5a582bcbe3
Simon Rupf <simon@rupf.net>
2020-09-19 09:31:30 +0200

    attempt using ncurses

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use std::env::args;
 use std::fs;
 use std::process::exit;
 
+extern crate pancurses;
+use pancurses::{initscr, endwin, Input, noecho, Window};
+use Input::*;
+
 fn main() {
     let path = match args().nth(1) {
         Some(path) => path,
@@ -21,4 +25,47 @@ fn main() {
     };
     println!("File {}:", path);
     println!("{}", content);
+
+    let window = initscr();
+    window.printw("File {} - Press Escape to quit\n");
+    window.addstr(content);
+    window.draw_box(0, 0);
+    window.refresh();
+    window.keypad(true);
+    noecho();
+    loop {
+        match window.getch() {
+            Some(key) if [KeyUp, KeyDown, KeyLeft, KeyRight].contains(&key) => handle_cursor_key(window, key),
+            Some(Character('\x1b')) => break, // escape key
+            Some(input) => { window.addstr(&format!("{:?}", input)); },
+            None => ()
+        }
+    }
+    endwin();
+}
+
+fn handle_cursor_key(&window: Window, key: Input) {
+    let x = window.get_cur_x();
+    let y = window.get_cur_y();
+    let mut updated_x = x;
+    let mut updated_y = y;
+
+    match key {
+        KeyUp => {
+            updated_y = y - 1;
+        },
+        KeyDown => {
+            updated_y = y + 1;
+        },
+        KeyLeft => {
+            updated_x = x - 1;
+        },
+        KeyRight => {
+            updated_x = x + 1;
+        },
+        _ => ()
+    }
+    window.mvwin(updated_y, updated_x);
+    window.mv(updated_y, updated_x);
+    window.refresh();
 }
